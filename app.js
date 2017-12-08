a1026b23fa30a6fe25f4f796769c9734e5896cbd
trumpetcoder <kevindjames55@gmail.com>
2017-12-07 18:39:30 -0700

    added racetrack, added comments to CSS

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 // Checked and working
 
 
-// Added keydown function move for top player
+// Added keydown function move for top player keydown == 39 is right arrow
 $(document).keydown(function(move) {
     if(move.keyCode == 39) {
         $('#topRacer').animate({marginLeft: "+=10" });
     }
 });
 
-// Added keydown function for bottom player
+// Added keydown function for bottom player keydown == 68 is D
 $(document).keydown(function(move) {
     if(move.keyCode == 68) {
         $('#botRacer').animate({marginLeft: "+=10" });
