aa7a857612dc781e29a87c1d1a8ccbb6f8b0d2be
trumpetcoder <kevindjames55@gmail.com>
2017-12-07 19:23:25 -0700

    Set up @media for tablet, background goes to white

--- app.js
+++ app.js
@@ -5,13 +5,13 @@
 // Added keydown function move for top player keydown == 39 is right arrow
 $(document).keydown(function(move) {
     if(move.keyCode == 39) {
-        $('#p1').animate({marginLeft: "+=10" });
+        $('#p1').animate({marginLeft: "+=5vw" });
     }
 });
 
 // Added keydown function for bottom player keydown == 68 is D
 $(document).keydown(function(move) {
     if(move.keyCode == 68) {
-        $('#p2').animate({marginLeft: "+=10" });
+        $('#p2').animate({marginLeft: "+=5vw" });
     }
 });
