04df46f44bba81821e3cd512e0897af7f3ecb133
trumpetcoder <kevindjames55@gmail.com>
2017-12-07 18:11:15 -0700

    Added move functionality in JS

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // Linked JS page
 // Checked and working
 
+
+// Added keydown function move for top player
+$(document).keydown(function(move) {
+    if(move.keyCode == 39) {
+        $('#topRacer').animate({marginLeft: "+=10" });
+    }
+});
+
+// Added keydown function for bottom player
+$(document).keydown(function(move) {
+    if(move.keyCode == 68) {
+        $('#botRacer').animate({marginLeft: "+=10" });
+    }
+});
