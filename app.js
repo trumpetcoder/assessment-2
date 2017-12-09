8e84c2e8c04388f54c51b2249c1c78f371d29f11
trumpetcoder <kevindjames55@gmail.com>
2017-12-09 12:25:15 -0700

    Added alerts for when players get to goal

--- app.js
+++ app.js
@@ -1,18 +1,60 @@
 // Linked JS page
 // Checked and working
 
+// Logic for the seperate racers to measure the distance from the edge
+var redBox = $(window).width() - ($('#p1').offset().left + $('#p1').outerWidth());
+var blueBox = $(window).width() - ($('#p2').offset().left + $('#p2').outerWidth());	
 
 // Added keydown function move for top player keydown == 39 is right arrow
 $(document).keydown(function(move) {
     if(move.keyCode == 39) {
-        $('#p1').animate({marginLeft: "+=2vw" });
-        	
+
+    	// Movement functionality P1
+        $('#p1').animate({left: '+=2vw'});
+        // console.log($('#p1')[0].style.left);
+        
+        var goal = ($('#p1')[0].style.left);
+       	
+       	var parseGoal = parseInt(goal);
+       	
+       	// console.log(parseGoal);
+
+        if (parseGoal >= 93) {
+			alert('Player 1 is Winner');
+
+
+        
+
+        
+
+       } 	
     }
 });
 
 // Added keydown function for bottom player keydown == 68 is D
 $(document).keydown(function(move) {
     if(move.keyCode == 68) {
-        $('#p2').animate({marginLeft: "+=2vw" });
+
+    	// Movement functionality P2
+        $('#p2').animate({left: '+=2vw' });
+
+        var goal = ($('#p2')[0].style.left);
+
+        var parseGoal = parseInt(goal);
+
+        // console.log(parseGoal);
+
+   	    if (parseGoal >= 93) {
+			alert('Player 2 is Winner');	 
+
+        
+        }
+        
     }
 });
+
+ 
+
+
+
+
